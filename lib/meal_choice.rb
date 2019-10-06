ffc19abc9719ecd685a3595b31dc26c5e46a72fb
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-06 19:43:50 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,14 @@
-def meal_choice (veg1, veg2, protein='meat')
-p  "What a nutritious meal!"  +  " A plate of #{protein} with #{veg1} and #{veg2}."
+def meal_choice(veg1, veg2, protein='meat')
+  p "What a nutritious meal!"
 end
-meal_choice("veg1", "veg2", "protein")
+
+def meal_choice (veg1, veg2, protein='meat')
+p  "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+meal_choice("broccoli", "macaroni")
+
+def meal_choice (veg1, veg2, protein='meat')
+p  "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+meal_choice("broccoli", "macaroni", "tofu")
+
